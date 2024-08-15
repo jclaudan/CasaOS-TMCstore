1fa3263d817ec7985e21c9a325189abcc0ee76c1
jclaudan <36130708+jclaudan@users.noreply.github.com>
2024-08-15 04:56:42 +0200

    Update start.sh

--- Apps/red-discordbot-shinsekai/start.sh
+++ Apps/red-discordbot-shinsekai/start.sh
@@ -6,8 +6,8 @@ Xvfb :99 -screen 0 1920x1080x24 &
 # Start VNC server for remote access
 x11vnc -display :99 -forever -nopw &
 
-# Start the LXDE desktop environment
-lxsession &
+# Start the XFCE4 desktop environment
+startxfce4 &
 
 # Start the Discord bot
 /app/start-redbot.sh
