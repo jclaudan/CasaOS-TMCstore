f60e7b4f625ce0872de9168de4ff2072ce59d38c
jclaudan <36130708+jclaudan@users.noreply.github.com>
2024-08-15 05:59:40 +0200

    Update start.sh

--- Apps/red-discordbot-shinsekai/start.sh
+++ Apps/red-discordbot-shinsekai/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Load v4l2loopback module for virtual camera support
+modprobe v4l2loopback devices=1 video_nr=10 card_label="OBS Virtual Camera" exclusive_caps=1
+
 # Start X virtual framebuffer in the background
 Xvfb :99 -screen 0 1920x1080x24 &
 
@@ -8,6 +11,9 @@ x11vnc -display :99 -forever -nopw &
 
 # Start the XFCE4 desktop environment
 startxfce4 &
+
+# Start OBS in minimized mode with Virtual Camera enabled
+obs --startvirtualcam --minimize-to-tray &
 
 # Start the Discord bot
 /app/start-redbot.sh
