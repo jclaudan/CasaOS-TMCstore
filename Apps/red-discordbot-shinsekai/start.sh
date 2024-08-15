ea4ccc9e21b6dd1aa57b6fb80a388c232f60261b
jclaudan <36130708+jclaudan@users.noreply.github.com>
2024-08-15 20:00:13 +0200

    Update start.sh 1

--- Apps/red-discordbot-shinsekai/start.sh
+++ Apps/red-discordbot-shinsekai/start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Check if a display server is already running
+# Démarrer Xvfb (serveur X virtuel)
 if ! pgrep -x "Xvfb" > /dev/null; then
     echo "Starting X virtual framebuffer..."
     Xvfb :99 -screen 0 1920x1080x24 &
     sleep 2  # Attendre que Xvfb démarre correctement
 else
-    echo "X virtual framebuffer already running."
+    echo "Xvfb is already running."
 fi
 
-# Check if a VNC server is already running
+# Démarrer le serveur VNC
 if ! pgrep -x "x11vnc" > /dev/null; then
     echo "Starting VNC server..."
     x11vnc -display :99 -forever -nopw &
 else
-    echo "VNC server already running."
+    echo "VNC server is already running."
 fi
 
-# Start the Discord bot
+# Démarrer le bot Discord
 echo "Starting Discord bot..."
 /app/start-redbot.sh
