0e5ea268b807a11aebd8b0821eb2416912593465
jclaudan <36130708+jclaudan@users.noreply.github.com>
2024-08-15 04:40:07 +0200

    Update start.sh

--- Apps/red-discordbot-shinsekai/start.sh
+++ Apps/red-discordbot-shinsekai/start.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 
-# # Start X virtual framebuffer in the background
-# Xvfb :99 -screen 0 1920x1080x24 &
-
-# # Start VNC server for remote access
-# x11vnc -display :99 -forever -nopw &
-
-# # Start OBS
-# obs --startvirtualcam --minimize-to-tray &
-
-# # Start VLC with a dummy interface (used for streaming)
-# cvlc --intf dummy &
-
-# # Start the Discord bot
-# /app/start-redbot.sh
-
-# ###
-
 # Start X virtual framebuffer in the background
 Xvfb :99 -screen 0 1920x1080x24 &
 
 # Start VNC server for remote access
 x11vnc -display :99 -forever -nopw &
 
-# Start a basic window manager (optional, but useful for VNC navigation)
-fluxbox &
-
-# Wait for Xvfb to start properly
-sleep 2
-
-# Optionally start OBS minimized to the tray (you can still control it via VNC)
-# Comment this out if you want to manually start OBS
-# obs --startvirtualcam --minimize-to-tray &
+# Start the LXDE desktop environment
+lxsession &
 
 # Start the Discord bot
 /app/start-redbot.sh
